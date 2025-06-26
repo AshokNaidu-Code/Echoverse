b3018ea0295abd45bee5c7f1c922a84bee0c55f2
Ashok22555 <ashoknallam03@gmail.com>
2025-06-26 22:33:26 +0530

    Github Action file created

--- server.js
+++ server.js
@@ -11,14 +11,14 @@ const PORT = process.env.PORT || 3000;
 const uri = process.env.MONGO_URI;
 const client = new MongoClient(uri);
 
-client
-  .connect()
+client.connect()
   .then(() => {
     console.log("✅ Connected to MongoDB successfully!");
   })
   .catch((err) => {
     console.error("❌ Failed to connect to MongoDB:", err.message);
   });
+
 
 app.use(cors());
 app.use(express.json());
